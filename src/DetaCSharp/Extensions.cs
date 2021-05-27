29c96bc14dfa38ee43dbabea8bc31979de27dc70
Fábio Ávila <fabio.avila@targetmp.com.br>
2021-05-27 13:52:59 -0300

    relocate projects

---
+++ src/DetaCSharp/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetaCSharp
+{
+    public static class Extensions
+    {
+        public static T[] Slice<T>(this T[] source, int start, int end)
+        {
+            if (end < 0)
+            {
+                end = source.Length + end;
+            }
+
+            int len = end - start;
+
+            T[] res = new T[len];
+            for (int i = 0; i < len; i++)
+            {
+                res[i] = source[i + start];
+            }
+            return res;
+        }
+    }
+}
